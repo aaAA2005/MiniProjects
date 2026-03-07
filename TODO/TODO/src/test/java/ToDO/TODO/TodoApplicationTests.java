911745e0a6132e0da5e75f53f7c4fde5f3da8c2d
aaAA2005 <ayxan19abbasov@gmail.com>
2026-03-07 18:50:09 +0400

    start

---
+++ TODO/TODO/src/test/java/ToDO/TODO/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package ToDO.TODO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
